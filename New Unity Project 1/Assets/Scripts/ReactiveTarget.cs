e25413a0bfb90d1ef1057e701bcd871d31e6ba38
pawii <pawii@yandex.ru>
2018-06-05 17:56:13 +0300

    first steps

---
+++ New Unity Project 1/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	private WanderingAI ai;
+
+	// Use this for initialization
+	void Start () {
+		ai = GetComponent<WanderingAI> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ReactToHit()
+	{
+		ai.SetAlive (false);
+		StartCoroutine (Die());
+	}
+
+	private IEnumerator Die()
+	{
+		transform.Rotate (-75, 0, 0);
+		yield return new WaitForSeconds (1.5f);
+		Destroy (this.gameObject);
+	}
+}
---
+++ New Unity Project 1/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour {
+
+	public float speed = 3f;
+	public float obstacleRange = 5f;
+
+	private bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAlive) {
+			transform.Translate (0, 0, speed * Time.deltaTime);
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+			if (Physics.SphereCast (ray, 0.75f, out hit))
+			if (hit.distance < obstacleRange)
+				transform.Rotate (0, Random.Range(-110, 110),0);
+		}
+	}
+
+	public void SetAlive(bool isAlive){
+		this.isAlive = isAlive;
+	}
+}
